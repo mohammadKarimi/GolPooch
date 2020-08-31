6aa978992b708776a29d7b4a14d54b3d8907c049
Mohammad K <dot.dotbydot@gmail.com>
2020-08-31 17:30:29 +0430

    Add Example of Kavenegar Send Sms

---
+++ GolPooch.Service/SmsStrategy.cs
@@ -0,0 +1,19 @@
+﻿using Kavenegar;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GolPooch.Service
+{
+    class SmsStrategy
+    {
+        public void Send()
+        {
+            var sender = "1000596446";
+            var receptor = "09393393079";
+            var message = ".وب سرویس پیام کوتاه کاوه نگار";
+            var api = new KavenegarApi("41636B3243496472392F72356834316954764D7231734564714F6446464B512F2B455230465379784455383D");
+            api.Send(sender, receptor, message);
+        }
+    }
+}
