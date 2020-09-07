a2568ad6f88fa67746917588e2246461887387fe
Mehran Norouzi <mehrannoruzi@gmail.com>
2020-09-07 19:44:08 +0430

    Add UserDeviceLog Entity & Create Host Database

--- GolPooch.DataAccess.Ef/Migrations/20200907145038_AddUserDeviceLog.Designer.cs
+++ GolPooch.DataAccess.Ef/Migrations/20200907145038_AddUserDeviceLog.Designer.cs
@@ -4,14 +4,16 @@ using GolPooch.DataAccess.Ef;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GolPooch.DataAccess.Ef.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200907145038_AddUserDeviceLog")]
+    partial class AddUserDeviceLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
