45da8353ac04f2906d9d3fa64395101cb86b8d3d
Mohammad K <dot.dotbydot@gmail.com>
2020-09-09 17:35:30 +0430

    change

--- GolPooch.Pwa/src/pages/auth/index.jsx
+++ GolPooch.Pwa/src/pages/auth/index.jsx
@@ -1,9 +1,23 @@
 ﻿import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
 
 const Authorization = () => {
     return (
         <div>
-            Hello Authorization !
+            <TextField
+                id="outlined-number"
+                label="Number"
+                type="text"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                variant="outlined"
+            />
+
+            <Button variant="contained" color="primary">
+                Disable elevation
+            </Button>
         </div>
     );
 }
