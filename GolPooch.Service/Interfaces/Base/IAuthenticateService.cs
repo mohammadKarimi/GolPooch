d1af5ea930a15782eeab3c96300f95698e39328d
Mehran Norouzi <mehrannoruzi@gmail.com>
2020-09-12 13:18:07 +0430

    Fixed Conflict

--- GolPooch.Service/Interfaces/Base/IAuthenticateService.cs
+++ GolPooch.Service/Interfaces/Base/IAuthenticateService.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace GolPooch.Service.Interfaces
 {
-    public interface IVerificationCodeService
+    public interface IAuthenticateService
     {
         /// <summary>
         /// Get verification Code + resend code again
@@ -12,14 +12,14 @@ namespace GolPooch.Service.Interfaces
         /// <param name="mobileNumber">MobileNumber was entered by user</param>
         /// <param name="osType">this field is for detecting which message we should send</param>
         /// <returns>return VerificationId for Verify</returns>
-        Task<IResponse<int>> GetCodeAsync(long mobileNumber, OsType osType);
+        Task<IResponse<int>> GetCodeAsync(long mobileNumber);
 
         /// <summary>
         /// verify last Code and register user with mobilenumber that user entered
         /// </summary>
-        /// <param name="verificationCodeId">the verification primaryKey for finding row</param>
-        /// <param name="code">the digit code that was entered by </param>
+        /// <param name="AuthenticateId">the verification primaryKey for finding row</param>
+        /// <param name="pinCode">the digit code that was entered by </param>
         /// <returns>User Model</returns>
-        Task<IResponse<User>> VerifyCodeAsync(int verificationCodeId, int pinCode);
+        Task<IResponse<User>> VerifyCodeAsync(int AuthenticateId, int pinCode);
     }
 }
